edb61f752d619bca46ba85b0d6702a1c0a43ca46
tandemresistentia <deathghoul41@gmail.com>
2024-12-30 18:13:29 +0100

    naming convetion fix

--- src/pages/api/useWaitlist.ts
+++ src/pages/api/useWaitlist.ts
@@ -4,15 +4,14 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -22,16 +21,30 @@ export const useWaitlist = () => {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const isValidEmail = (email : string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
   const handleJoinWaitlist = async () => {
     setLoading(true);
     setMessage("");
 
     try {
+      if (!email.trim()) {
+        setMessage("Email is required.");
+        return;
+      }
+
+      if (!isValidEmail(email)) {
+        setMessage("Please enter a valid email address.");
+        return;
+      }
       // Save email to Firestore
       await addDoc(collection(db, "waitlist"), { email, createdAt: new Date() });
 
       setMessage("Thank you for signing up!");
-      setEmail("");
+      setEmail(""); // Clear the email input after submission
     } catch (error) {
       console.error("Error saving email: ", error);
       setMessage("An unexpected error occurred. Please try again later.");
